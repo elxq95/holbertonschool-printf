12efc78cd1c43b7f722889d6b06042d3028babc4
double0kung <00kung@gmail.com>
2024-07-25 12:02:28 +0000

    fix: issue with _printf(%\)

--- _printf.c
+++ _printf.c
@@ -10,34 +10,25 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i;
-	int size;
-	int flagged;
+	int i, size = 0;
 
 	if (format == NULL)
 		return (-1);
 
 	va_start(args, format);
-	size = 0;
-	flagged = 0;
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (flagged == 0)
+		if (format[i] == '%')
 		{
-			if (format[i] == '%')
-			{
-				flagged = 1;
-			}
-			else
-			{
-				print_normal(format[i], &size);
-			}
+			if (format[i + 1] == '\0')
+				return (-1);
+			i++;
+			pick_operation(format[i], args, &size);
 		}
 		else
 		{
-			pick_operation(format[i], args, &size);
-			flagged = 0;
+			print_normal(format[i], &size);
 		}
 	}
 
--- pick_operation.c
+++ pick_operation.c
@@ -3,28 +3,27 @@
 #include <stdio.h>
 
 /**
- * pick_operation - selects the correct print function based on the format specifier
+ * pick_operation - selects print function based on format specifier
  * @flag: the format specifier
  * @args: list of arguments
  * @size: pointer to the size counter
  */
 void pick_operation(char flag, va_list args, int *size)
 {
-    switch (flag)
-    {
-    case 'c':
-        print_char(args, size);
-        break;
-    case 's':
-        print_string(args, size);
-        break;
-    case '%':
-        print_percent(size);
-        break;
-    default:
-	putchar('%');
-	putchar(flag);
-	*size += 2;
-        break;
-    }
+	switch (flag)
+	{
+		case 'c':
+			print_char(args, size);
+			break;
+		case 's':
+			print_string(args, size);
+			break;
+		case '%':
+			print_percent(size);
+			break;
+		default:
+			print_normal('%', size);
+			print_normal(flag, size);
+			break;
+	}
 }
